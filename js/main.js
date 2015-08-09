aa8d46d3ff318dba341bc0a7f571a228d58d4b9e
Nathan-Bateman <nathan.bateman.jr@gmail.com>
2015-08-09 16:59:30 -0500

    added in correct links to portfolio pages

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var HTMLfooterContact = "</li><li><a href='https://www.linkedin.com/pub/nathan-bateman/ab/191/895'><img class='image-circle' src='images/linkedin.png' width='64px' height='64px' alt='linkedin'></a>" +
+                        "<li><a href='https://github.com/Nathan-Bateman'><img class='image-circle' src='images/GitHub-Mark-64px.png' alt='github'></a>" +
+                        "</li><li><a href='mailto:nathan.bateman.jr@gmail.com'><img class='image-circle' src='images/email.png' width='64px' height='64px' alt='email'></li></a>";
+
+$("#footerContacts").append(HTMLfooterContact);                     
